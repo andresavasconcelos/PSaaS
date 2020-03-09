9bbd72e14c5f20d017357f8a1dc70da577f94641
fabioluisaf <fabioluizflaf@gmail.com>
2020-03-09 16:40:42 -0300

    Finalizada a função da parte de projeto

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -12,10 +12,13 @@ public class Principal {
 		ArrayList<String> requisitosClasse2 = montaListaRequisitos(7, 5);
 		
 		printFaseRequisitos(requisitosClasse1, requisitosClasse2);
+		
 		printInicioFaseProjeto(requisitosClasse1, requisitosClasse2);
 		
 		Classe1 c1 = new Classe1(requisitosClasse1);
 		Classe2 c2 = new Classe2(requisitosClasse2);
+		
+		System.out.println("Fim da fase de projeto.\n\n");
 	}
 	
 	public static ArrayList<String> montaListaRequisitos(int numInicial, int quantidade) {
@@ -36,7 +39,7 @@ public class Principal {
 		
 		System.out.println("Recebendo requisitos...");
 		System.out.println("Os requisitos recebidos sao: " + requisitosTotal);
-		System.out.println("Fim da coleta de requisitos\n\n");
+		System.out.println("Fim da coleta de requisitos.\n\n");
 	}
 	
 	public static void printInicioFaseProjeto(ArrayList<String> requisitos1, ArrayList<String> requisitos2) {
@@ -44,8 +47,6 @@ public class Principal {
 		
 		System.out.println("Os requisitos: " + requisitos1 + " serao associados a classe: " + Classe1.class);
 		System.out.println("Os requisitos: " + requisitos2 + " serao associados a classe: " + Classe2.class);
-		
-		
 	}
 }
 
