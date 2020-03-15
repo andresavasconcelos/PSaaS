1ce4f7443e9b195a824c55a4c57cb2483e221dd4
fabioluisaf <fabioluizflaf@gmail.com>
2020-03-15 15:09:30 -0300

    Update TesteUnidade.java

--- src/modelo/fases/TesteUnidade.java
+++ src/modelo/fases/TesteUnidade.java
@@ -14,8 +14,8 @@ public class TesteUnidade {
 	public static void rodaTeste(Class<Classe1> c1, Class<Classe2> c2, ArrayList<String> reqC1, ArrayList<String> reqC2) {
 		printAndWait("Iniciando testes de unidade...", 500);
 		
-		// pegando todos os metodos das classes testadas
 		printAndWait("Coletando metodos das classes...", 500);
+		// pegando todos os metodos das classes testadas
 		ArrayList<Method> metodosClasse1 = new ArrayList<>(Arrays.asList(c1.getDeclaredMethods()));
 		ArrayList<Method> metodosClasse2 = new ArrayList<>(Arrays.asList(c2.getDeclaredMethods()));
 		
@@ -24,6 +24,15 @@ public class TesteUnidade {
 		Classe1 c1Instancia = new Classe1(reqC1);
 		Classe2 c2Instancia = new Classe2(reqC2);
 		
+		/*
+		 * O metodo invoke() da classe Method pode lancar algumas excecoes:
+		 * 	IllegalAccessException: quando tentamos invocar um metodo que nao temos acesso, por exemplo um metodo privado
+		 *  IllegalArgumentException: quando os argumentos que passamos estao incorretos
+		 *  InvocationTargetException: alguma excecao lancada pelo metodo original
+		 *  
+		 * Como nesse codigo nenhuma delas pode ocorrer, nao ha necessidade de tratar as excecoes, pois elas nunca serao
+		 * lancadas, o try-catch esta ai pois o codigo nao compilaria sem ele.
+		 */
 		try {
 			printAndWait("\n\nIniciando testes da classe " + c1, 500);
 			
@@ -49,6 +58,7 @@ public class TesteUnidade {
 		printAndWait("\nIniciando testes no metodo " + metodo.getName() + "...", 250);
 	
 		for(int i = 0; i < qtdCasos; i++) {
+			// chama o metodo em uma determinada instancia da classe
 			metodo.invoke(instancia);
 			System.out.println("Caso de teste " + i + " no metodo " + metodo.getName() + " executou corretamente.");
 		}
